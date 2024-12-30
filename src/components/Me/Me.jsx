5203447d618a069a0e565a26e130f191fa33a186
coderx86 <supurnadewan8729@gmail.com>
2024-12-30 11:47:01 +0600

    added Me

---
+++ src/components/Me/Me.jsx
@@ -0,0 +1,34 @@
+import { useContext } from "react";
+import { authContext } from "../../layout/MainLayout";
+
+const Me = () => {
+  const { user, loading } = useContext(authContext);
+  console.log(user);
+  return (
+    <div className="flex justify-center pt-24 bg-gradient-to-b from-[#58b58d44] to-[#f1ab4a3b]">
+      <div className="my-16 py-16 border-[#58b58c] border-2 w-[500px] rounded-xl">
+        <div className="flex justify-center mb-6">
+          {loading ? (
+            <span className="loading loading-ring loading-md"></span>
+          ) : user ? (
+            <img
+              src={user.photoURL}
+              alt="User Profile"
+              className="size-[150px] rounded-full"
+            />
+          ) : null}
+        </div>
+        <div>
+          <h2 className="text-2xl font-bold text-center mb-2">
+            Hey! I'm<span className="text-[#fd7379]"> {user.displayName}</span>
+          </h2>
+          <h2 className="text-xl text-center text-gray-600">
+            <span className="font-bold">Email:</span> {user.email}
+          </h2>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Me;
